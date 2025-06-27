8e011ee6222eb77fa57b9b2279364e5663817031
Rushita Ghinaiya <rushita.ghinaiya@themedibank.com>
2025-06-27 17:25:13 +0530

    Add smarch search in medicine list

--- ChatBot/Repository/MedicineRepository.cs
+++ ChatBot/Repository/MedicineRepository.cs
@@ -20,47 +20,57 @@ namespace ChatBot.Repository
             using (MySqlConnection connection = new MySqlConnection(_connectionString))
             {
                 connection.Open();
+
                 try
                 {
                     var offset = (page - 1) * pageSize;
                     var whereClause = includeDiscontinued ? "" : "AND (Is_discontinued = 0 OR Is_discontinued IS NULL)";
 
+
+                    var searchWords = name.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    var searchLike = "%" + string.Join("%", searchWords) + "%";
+                    var exactName = name + "%";
+
                     var countSql = $@"
-                        SELECT COUNT(*) 
-                        FROM medicinedata 
-                        WHERE name LIKE @SearchName 
-                        {whereClause}";
+                SELECT COUNT(*) 
+                FROM medicinedata 
+                WHERE LOWER(name) LIKE @SearchName 
+                {whereClause}";
 
                     var sql = $@"
-                        SELECT 
-                            id as Id,
-                            name as Name,
-                            price as Price,
-                            Is_discontinued as IsDiscontinued,
-                            manufacturer_name as ManufacturerName,
-                            type as Type,
-                            pack_size_label as PackSizeLabel,
-                            short_composition1 as ShortComposition1,
-                            short_composition2 as ShortComposition2,
-                            salt_composition as SaltComposition,
-                            medicine_desc as MedicineDescription,
-                            side_effects as SideEffects,
-                            drug_interactions as DrugInteractions
-                        FROM medicinedata 
-                        WHERE name LIKE @SearchName 
-                        {whereClause}
-                        ORDER BY 
-                            CASE WHEN name LIKE @ExactName THEN 1 ELSE 2 END,
-                            name
-                        LIMIT @PageSize OFFSET @Offset";
+                SELECT 
+                    id AS Id,
+                    name AS Name,
+                    price AS Price,
+                    Is_discontinued AS IsDiscontinued,
+                    manufacturer_name AS ManufacturerName,
+                    type AS Type,
+                    pack_size_label AS PackSizeLabel,
+                    short_composition1 AS ShortComposition1,
+                    short_composition2 AS ShortComposition2,
+                    salt_composition AS SaltComposition,
+                    medicine_desc AS MedicineDescription,
+                    side_effects AS SideEffects,
+                    drug_interactions AS DrugInteractions
+                FROM medicinedata 
+                WHERE LOWER(name) LIKE @SearchName 
+                {whereClause}
+                ORDER BY 
+                    CASE 
+                        WHEN LOWER(name) LIKE @ExactName THEN 1 
+                        ELSE 2 
+                    END,
+                    name
+                LIMIT @PageSize OFFSET @Offset";
 
-                    var searchName = $"%{name}%";
-                    var exactName = $"{name}%";
+                    var totalCount = await connection.QuerySingleAsync<int>(countSql, new
+                    {
+                        SearchName = searchLike
+                    });
 
-                    var totalCount = await connection.QuerySingleAsync<int>(countSql, new { SearchName = searchName });
                     var medicines = await connection.QueryAsync<MedicineSearchVM>(sql, new
                     {
-                        SearchName = searchName,
+                        SearchName = searchLike,
                         ExactName = exactName,
                         PageSize = pageSize,
                         Offset = offset
